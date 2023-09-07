55a16beb40ee006c3ce3ad6f4b062f59c0826238
anshumansinha18 <sinhaanshuman18298@gmail.com>
2023-09-08 02:53:49 +0530

    boilerplate code added

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const questions = [
+  {
+    text: "Which language is primarily used for web app development?",
+    options: ["C#", "Python", "JavaScript", "Swift"],
+    correct: 2,
+  },
+  {
+    text: "Which of the following is a relational database management system?",
+    options: ["Oracle", "Scala", "Perl", "Java"],
+    correct: 0,
+  },
+  {
+    text: "In which language is memory management provided by JVM?",
+    options: ["Java", "C", "C++", "Python"],
+    correct: 0,
+  },
+  {
+    text: "What does HTML stand for?",
+    options: [
+      "Hyperlink and Text Markup Language",
+      "High Technology Modern Language",
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+    ],
+    correct: 2,
+  },
+];
+
+
+
